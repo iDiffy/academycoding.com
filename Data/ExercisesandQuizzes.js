54a00218b7b82566283151ac39c9d1b0e231bbf8
iDiffy <126216900+iDiffy@users.noreply.github.com>
2024-01-12 17:00:04 +0800

    Add files via upload

---
+++ Data/ExercisesandQuizzes.js
@@ -0,0 +1,407 @@
+export const ExercisesandQuizzes = {
+    name : "Latihan dan Kuiz",
+    Exercises : {
+        name : "Latihan",
+        menu : [
+            {
+                title : "Latihan HTML",
+                link : "https://www.w3schools.com/html/html_exercises.asp"
+            },
+            {
+                title : "Latihan CSS",
+                link : "https://www.w3schools.com/css/css_exercises.asp"
+            },
+            {
+                title : "Latihan JavaScript",
+                link : "https://www.w3schools.com/js/js_exercises.asp"
+            },
+            {
+                title : "Latihan Python",
+                link : "https://www.w3schools.com/python/python_exercises.asp"
+            },
+            {
+                title : "Latihan SQL",
+                link : "https://www.w3schools.com/sql/sql_exercises.asp"
+            },
+            {
+                title : "Latihan PHP",
+                link : "https://www.w3schools.com/php/php_exercises.asp"
+            },
+            {
+                title : "Latihan Java",
+                link : "https://www.w3schools.com/java/java_exercises.asp"
+            },
+            {
+                title : "Latihan C",
+                link : "https://www.w3schools.com/c/c_exercises.php"
+            },
+            {
+                title : "Latihan C++",
+                link : "https://www.w3schools.com/cpp/cpp_exercises.asp"
+            },
+            {
+                title : "Latihan C#",
+                link : "https://www.w3schools.com/cs/cs_exercises.php"
+            },
+            {
+                title : "Latihan jQuery",
+                link : "https://www.w3schools.com/jquery/jquery_exercises.asp"
+            },
+            {
+                title : "Latihan React.js",
+                link : "https://www.w3schools.com/react/react_exercises.asp"
+            },
+            {
+                title : "Latihan MySQL",
+                link : "https://www.w3schools.com/mysql/mysql_exercises.asp"
+            },
+            {
+                title : "Latihan Bootstrap 5",
+                link : "https://www.w3schools.com/bootstrap5/index.php"
+            },
+            {
+                title : "Latihan Bootstrap 4",
+                link : "https://www.w3schools.com/bootstrap4/default.asp"
+            },
+            {
+                title : "Latihan Bootstrap 3",
+                link : "https://www.w3schools.com/bootstrap/bootstrap_exercises.asp"
+            },
+            {
+                title : "Latihan NumPy",
+                link : "https://www.w3schools.com/python/numpy/numpy_exercises.asp"
+            },
+            {
+                title : "Latihan Pandas",
+                link : "https://www.w3schools.com/python/pandas/pandas_exercises.asp"
+            },
+            {
+                title : "Latihan SciPy",
+                link : "https://www.w3schools.com/python/scipy/scipy_exercises.php"
+            },
+            {
+                title : "Latihan TypeScript",
+                link : "https://www.w3schools.com/typescript/typescript_exercises.php"
+            },
+            {
+                title : "Latihan Excel",
+                link : "https://w3schools.com/excel/excel_exercises.php"
+            },
+            {
+                title : "Latihan R",
+                link : "https://www.w3schools.com/r/r_exercises.asp"
+            },
+            {
+                title : "Latihan Git",
+                link : "https://www.w3schools.com/git/git_exercises.asp"
+            },
+            {
+                title : "Latihan Kotlin",
+                link : "https://www.w3schools.com/kotlin/kotlin_exercises.php"
+            },
+            {
+                title : "Latihan Go",
+                link : "https://www.w3schools.com/go/go_exercises.php"
+            },
+            {
+                title : "Latihan MongoDB",
+                link : "https://www.w3schools.com/mongodb/mongodb_exercises.php"
+            },
+        ]
+    },
+    Quizzes : {
+        name : "Kuiz",
+        menu : [
+            {
+                title : "Kuiz HTML",
+                link : "https://www.w3schools.com/html/html_quiz.asp"
+            },
+            {
+                title : "Kuiz CSS",
+                link : "https://www.w3schools.com/css/css_quiz.asp"
+            },
+            {
+                title : "Kuiz JavaScript",
+                link : "https://www.w3schools.com/js/js_quiz.asp"
+            },
+            {
+                title : "Kuiz Python",
+                link : "https://www.w3schools.com/python/python_quiz.asp"
+            },
+            {
+                title : "Kuiz SQL",
+                link : "https://www.w3schools.com/sql/sql_quiz.asp"
+            },
+            {
+                title : "Kuiz PHP",
+                link : "https://www.w3schools.com/php/php_quiz.asp"
+            },
+            {
+                title : "Kuiz Java",
+                link : "https://www.w3schools.com/java/java_quiz.asp"
+            },
+            {
+                title : "Kuiz C",
+                link : "https://www.w3schools.com/c/c_quiz.php"
+            },
+            {
+                title : "Kuiz C++",
+                link : "https://www.w3schools.com/cpp/cpp_quiz.asp"
+            },
+            {
+                title : "Kuiz C#",
+                link : "https://www.w3schools.com/cs/cs_quiz.php"
+            },
+            {
+                title : "Kuiz jQuery",
+                link : "https://www.w3schools.com/jquery/jquery_quiz.asp"
+            },
+            {
+                title : "Kuiz React.js",
+                link : "https://www.w3schools.com/react/react_quiz.asp"
+            },
+            {
+                title : "Kuiz MySQL",
+                link : "https://www.w3schools.com/mysql/mysql_quiz.asp"
+            },
+            {
+                title : "Kuiz Bootstrap 5",
+                link : "https://www.w3schools.com/bootstrap5/bootstrap_quiz.php"
+            },
+            {
+                title : "Kuiz Bootstrap 4",
+                link : "https://www.w3schools.com/bootstrap4/bootstrap_quiz.asp"
+            },
+            {
+                title : "Kuiz Bootstrap 3",
+                link : "https://www.w3schools.com/bootstrap/bootstrap_quiz.asp"
+            },
+            {
+                title : "Kuiz NumPy",
+                link : "https://www.w3schools.com/python/numpy/numpy_quiz.asp"
+            },
+            {
+                title : "Kuiz Pandas",
+                link : "https://www.w3schools.com/python/pandas/pandas_quiz.asp"
+            },
+            {
+                title : "Kuiz SciPy",
+                link : "https://www.w3schools.com/python/scipy/scipy_quiz.php"
+            },
+            {
+                title : "Kuiz TypeScript",
+                link : "https://www.w3schools.com/typescript/typescript_quiz.php"
+            },
+            {
+                title : "Kuiz XML",
+                link : "https://www.w3schools.com/xml/xml_quiz.asp"
+            },
+            {
+                title : "Kuiz R",
+                link : "https://www.w3schools.com/r/r_quiz.asp"
+            },
+            {
+                title : "Kuiz Git",
+                link : "https://www.w3schools.com/git/git_quiz.asp"
+            },
+            {
+                title : "Kuiz Kotlin",
+                link : "https://www.w3schools.com/kotlin/kotlin_quiz.php"
+            },
+            {
+                title : "Kuiz Cyber Security",
+                link : "https://www.w3schools.com/quiztest/quiztest.php?qtest=CYBERSECURITY"
+            },
+            {
+                title : "Kuiz Accessibility",
+                link : "https://www.w3schools.com/quiztest/quiztest.php?qtest=ACCESSIBILITY"
+            },
+        ]
+    },
+    Courses : {
+        name : "Kursus",
+        menu : [
+            {
+                title : "Kursus HTML",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/html-course"
+            },
+            {
+                title : "Kursus CSS",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/css-course"
+            },
+            {
+                title : "Kursus JavaScript",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/javascript-course"
+            },
+            {
+                title : "Kursus Front End",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/front-end-course"
+            },
+            {
+                title : "Kursus Python",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/python-course"
+            },
+            {
+                title : "Kursus SQL",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/sql-course"
+            },
+            {
+                title : "Kursus PHP",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/php-course"
+            },
+            {
+                title : "Kursus Java",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/java-course"
+            },
+            {
+                title : "Kursus C++",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/c-course-1"
+            },
+            {
+                title : "Kursus C#",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/c-course"
+            },
+            {
+                title : "Kursus jQuery",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/jquery-course"
+            },
+            {
+                title : "Kursus React.js",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/react-js-course"
+            },
+            {
+                title : "Kursus Bootstrap 4",
+                link : "https://campus.w3schools.com/collections/single-courses/products/bootstrap-4-course"
+            },
+            {
+                title : "Kursus Bootstrap 3",
+                link : "https://campus.w3schools.com/collections/single-courses/products/bootstrap-course"
+            },
+            {
+                title : "Kursus NumPy",
+                link : "https://campus.w3schools.com/products/numpy-course"
+            },
+            {
+                title : "Kursus Pandas",
+                link : "https://campus.w3schools.com/products/pandas-course"
+            },
+            {
+                title : "Kursus TypeScript",
+                link : "https://campus.w3schools.com/products/learn-typescript"
+            },
+            {
+                title : "Kursus XML",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/xml-course"
+            },
+            {
+                title : "Kursus R",
+                link : "https://campus.w3schools.com/collections/course-catalog/products/r-course"
+            },
+            {
+                title : "Kursus Data Analytics",
+                link : "https://campus.w3schools.com/products/data-analytics-program"
+            },
+            {
+                title : "Kursus Cyber Security",
+                link : "https://campus.w3schools.com/products/cyber-security-course"
+            },
+            {
+                title : "Kursus Accessibility",
+                link : "https://campus.w3schools.com/products/accessibility-course"
+            },
+        ]
+    },
+    Certificates : {
+        name : "Sijil",
+        menu : [
+            {
+                title : "Sijil HTML",
+                link : "https://campus.w3schools.com/collections/certifications/products/html-certificate"
+            },
+            {
+                title : "Sijil CSS",
+                link : "https://campus.w3schools.com/collections/certifications/products/css-certificate"
+            },
+            {
+                title : "Sijil JavaScript",
+                link : "https://campus.w3schools.com/collections/certifications/products/javascript-certificate"
+            },
+            {
+                title : "Sijil Front End",
+                link : "https://campus.w3schools.com/collections/certifications/products/front-end-certificate"
+            },
+            {
+                title : "Sijil Python",
+                link : "https://campus.w3schools.com/collections/certifications/products/python-certificate"
+            },
+            {
+                title : "Sijil SQL",
+                link : "https://campus.w3schools.com/collections/certifications/products/sql-certificate"
+            },
+            {
+                title : "Sijil PHP",
+                link : "https://campus.w3schools.com/collections/certifications/products/php-certificate"
+            },
+            {
+                title : "Sijil Java",
+                link : "https://campus.w3schools.com/collections/certifications/products/java-certificate"
+            },
+            {
+                title : "Sijil C++",
+                link : "https://campus.w3schools.com/collections/certifications/products/c-certificate"
+            },
+            {
+                title : "Sijil C#",
+                link : "https://campus.w3schools.com/collections/certifications/products/c-certificate-1"
+            },
+            {
+                title : "Sijil jQuery",
+                link : "https://campus.w3schools.com/collections/certifications/products/jquery-certificate"
+            },
+            {
+                title : "Sijil React.js",
+                link : "https://campus.w3schools.com/collections/certifications/products/react-js-certificate"
+            },
+            {
+                title : "Sijil MySQL",
+                link : "https://campus.w3schools.com/collections/certifications/products/mysql-certificate"
+            },
+            {
+                title : "Sijil Bootstrap 5",
+                link : "https://campus.w3schools.com/collections/certifications/products/bootstrap-5-certificate"
+            },
+            {
+                title : "Sijil Bootstrap 4",
+                link : "https://campus.w3schools.com/collections/certifications/products/bootstrap-4-certificate"
+            },
+            {
+                title : "Sijil Bootstrap 3",
+                link : "https://campus.w3schools.com/collections/certifications/products/bootstrap-3-certificate"
+            },
+            {
+                title : "Sijil TypeScript",
+                link : "https://campus.w3schools.com/collections/certifications/products/typescript-certificate"
+            },
+            {
+                title : "Sijil XML",
+                link : "https://campus.w3schools.com/collections/certifications/products/xml-certificate"
+            },
+            {
+                title : "Sijil Excel",
+                link : "https://campus.w3schools.com/products/excel-certificate"
+            },
+            {
+                title : "Sijil Data Science",
+                link : "https://campus.w3schools.com/collections/certifications/products/data-science-certificate"
+            },
+            {
+                title : "Sijil Cyber Security",
+                link : "https://campus.w3schools.com/collections/certifications/products/cyber-security-certificate"
+            },
+            {
+                title : "Sijil Accessibility",
+                link : "https://campus.w3schools.com/collections/certifications/products/accessibility-certificate"
+            },
+        ]
+    },
+}
---
+++ Data/Tutorials.js
@@ -0,0 +1,320 @@
+export const Tutorials = {
+    name : "Panduan",
+    HTMLAndCSS : {
+        name : "HTML dan CSS",
+        menu : [
+            {
+                title : "Belajar HTML",
+                link : "https://www.w3schools.com/html/default.asp"
+            },
+            {
+                title : "Belajar CSS",
+                link : "https://www.w3schools.com/css/default.asp"
+            },
+            {
+                title : "Belajar RWD",
+                link : "https://www.w3schools.com/css/css_rwd_intro.asp"
+            },
+            {
+                title : "Belajar Bootstrap",
+                link : "https://www.w3schools.com/bootstrap/bootstrap_ver.asp"
+            },
+            {
+                title : "Belajar W3.CSS",
+                link : "https://www.w3schools.com/w3css/default.asp"
+            },
+            {
+                title : "Belajar Colors",
+                link : "https://www.w3schools.com/colors/default.asp"
+            },
+            {
+                title : "Belajar Icons",
+                link : "https://www.w3schools.com/icons/default.asp"
+            },
+            {
+                title : "Belajar Graphics",
+                link : "https://www.w3schools.com/graphics/default.asp"
+            },
+            {
+                title : "Belajar SVG",
+                link : "https://www.w3schools.com/graphics/svg_intro.asp"
+            },
+            {
+                title : "Belajar Canvas",
+                link : "https://www.w3schools.com/graphics/canvas_intro.asp"
+            },
+            {
+                title : "Belajar How To",
+                link : "https://www.w3schools.com/howto/default.asp"
+            },
+            {
+                title : "Learn Sass",
+                link : "https://www.w3schools.com/sass/default.php"
+            },
+        ]
+    },
+    JavaScript : {
+        name : "JavaScript",
+        menu : [
+            {
+                title : "Belajar JavaScript",
+                link : "https://www.w3schools.com/js/default.asp"
+            },
+            {
+                title : "Belajar jQuery",
+                link : "https://www.w3schools.com/jquery/default.asp"
+            },
+            {
+                title : "Belajar React",
+                link : "https://www.w3schools.com/react/default.asp"
+            },
+            {
+                title : "Belajar AngularJS",
+                link : "https://www.w3schools.com/angular/default.asp"
+            },
+            {
+                title : "Belajar JSON",
+                link : "https://www.w3schools.com/js/js_json_intro.asp"
+            },
+            {
+                title : "Belajar AJAX",
+                link : "https://www.w3schools.com/js/js_json_intro.asp"
+            },
+            {
+                title : "Belajar AppML",
+                link : "https://www.w3schools.com/appml/default.asp"
+            },
+            {
+                title : "Belajar W3.JS",
+                link : "https://www.w3schools.com/w3js/default.asp"
+            },
+        
+        
+        
+        
+        ]
+    },
+    Programming : {
+        name : "Programming",
+        menu : [
+            {
+                title : "Belajar Python",
+                link : "https://www.w3schools.com/python/default.asp"
+            },
+            {
+                title : "Belajar Java",
+                link : "https://www.w3schools.com/java/default.asp"
+            },
+            {
+                title : "Belajar C",
+                link : "https://www.w3schools.com/c/index.php"
+            },
+            {
+                title : "Belajar C++",
+                link : "https://www.w3schools.com/cpp/default.asp"
+            },
+            {
+                title : "Belajar C#",
+                link : "https://www.w3schools.com/cs/index.php"
+            },
+            {
+                title : "Belajar R",
+                link : "https://www.w3schools.com/r/default.asp"
+            },
+            {
+                title : "Belajar Kotlin",
+                link : "https://www.w3schools.com/kotlin/index.php"
+            },
+            {
+                title : "Belajar Go",
+                link : "https://www.w3schools.com/go/index.php"
+            },
+            {
+                title : "Belajar Django",
+                link : "https://www.w3schools.com/django/index.php"
+            },
+            {
+                title : "Belajar TypeScript",
+                link : "https://www.w3schools.com/typescript/index.php"
+            },
+        
+        
+        
+        
+        ]
+    },
+    ServerSide : {
+        name : "Server Side",
+        menu : [
+            {
+                title : "Belajar SQL",
+                link : "https://www.w3schools.com/sql/default.asp"
+            },
+            {
+                title : "Belajar MySQL",
+                link : "https://www.w3schools.com/mysql/default.asp"
+            },
+            {
+                title : "Belajar PHP",
+                link : "https://www.w3schools.com/php/default.asp"
+            },
+            {
+                title : "Belajar ASP",
+                link : "https://www.w3schools.com/asp/default.asp"
+            },
+            {
+                title : "Belajar Node.js",
+                link : "https://www.w3schools.com/nodejs/default.asp"
+            },
+            {
+                title : "Belajar Raspberry Pi",
+                link : "https://www.w3schools.com/nodejs/nodejs_raspberrypi.asp"
+            },
+            {
+                title : "Belajar Git",
+                link : "https://www.w3schools.com/git/default.asp"
+            },
+            {
+                title : "Belajar MongoDB",
+                link : "https://www.w3schools.com/mongodb/index.php"
+            },
+            {
+                title : "Belajar AWS Cloud",
+                link : "https://www.w3schools.com/aws/index.php"
+            },
+        
+        
+        ]
+    },
+    WebBuilding : {
+        name : "Pembinaan Laman Web",
+        menu : [
+            {
+                title : "Cipta Laman Web",
+                link : "https://www.w3schools.com/spaces/index.php"
+            },
+            {
+                title : "Di Mana Untuk Mula",
+                link : "https://www.w3schools.com/where_to_start.asp"
+            },
+            {
+                title : "Templat Laman Web",
+                link : "https://www.w3schools.com/w3css/w3css_templates.asp"
+            },
+            {
+                title : "Statistik Web",
+                link : "https://www.w3schools.com/browsers/default.asp"
+            },
+            {
+                title : "Sijil Web",
+                link : "https://campus.w3schools.com/"
+            },
+            {
+                title : "Editor Kod",
+                link : "https://www.w3schools.com/tryit/default.asp"
+            },
+            {
+                title : "Uji Kelajuan Menaip Anda",
+                link : "https://www.w3schools.com/typingspeed/default.asp"
+            },
+            {
+                title : "Bermain Permainan Kod",
+                link : "https://www.w3schools.com/codegame/index.html"
+            },
+            {
+                title : "Keselamatan Siber",
+                link : "https://www.w3schools.com/cybersecurity/index.php"
+            },
+            {
+                title : "Capaian",
+                link : "https://www.w3schools.com/accessibility/index.php"
+            },
+            {
+                title : "Sertai Buletin Kami",
+                link : "https://campus.w3schools.com/pages/newsletter"
+            },
+        ]
+    },
+    DataAnalytics : {
+        name : "Data Analytics",
+        menu : [
+            {
+                title : "Belajar AI",
+                link : "https://www.w3schools.com/ai/default.asp"
+            },
+            {
+                title : "Belajar Machine Learning",
+                link : "https://www.w3schools.com/python/python_ml_getting_started.asp"
+            },
+            {
+                title : "Belajar Data Science",
+                link : "https://www.w3schools.com/datascience/default.asp"
+            },
+            {
+                title : "Belajar NumPy",
+                link : "https://www.w3schools.com/python/numpy/default.asp"
+            },
+            {
+                title : "Belajar Pandas",
+                link : "https://www.w3schools.com/python/pandas/default.asp"
+            },
+            {
+                title : "Belajar SciPy",
+                link : "https://www.w3schools.com/python/scipy/index.php"
+            },
+            {
+                title : "Belajar Matplotlib",
+                link : "https://www.w3schools.com/python/matplotlib_intro.asp"
+            },
+            {
+                title : "Belajar Statistics",
+                link : "https://www.w3schools.com/statistics/index.php"
+            },
+            {
+                title : "Belajar Excel",
+                link : "https://www.w3schools.com/excel/index.php"
+            },
+            {
+                title : "Belajar Google Sheets",
+                link : "https://www.w3schools.com/googlesheets/index.php"
+            },
+        ]
+    },
+    XMLTutorials : {
+        name : "Panduan XML",
+        menu : [
+            {
+                title : "Belajar XML",
+                link : "https://www.w3schools.com/xml/default.asp"
+            },
+            {
+                title : "Belajar XML AJAX",
+                link : "https://www.w3schools.com/xml/ajax_intro.asp"
+            },
+            {
+                title : "Belajar XML DOM",
+                link : "https://www.w3schools.com/xml/xml_dom.asp"
+            },
+            {
+                title : "Belajar XML DTD",
+                link : "https://www.w3schools.com/xml/xml_dtd.asp"
+            },
+            {
+                title : "Belajar XML Schema",
+                link : "https://www.w3schools.com/xml/xml_schema.asp"
+            },
+            {
+                title : "Belajar XSLT",
+                link : "https://www.w3schools.com/xml/xml_xslt.asp"
+            },
+            {
+                title : "Belajar XPath",
+                link : "https://www.w3schools.com/xml/xml_xpath.asp"
+            },
+            {
+                title : "Belajar XQuery",
+                link : "https://www.w3schools.com/xml/xml_xquery.asp"
+            },
+        ]
+    }, 
+}
